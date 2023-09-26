70b3020e245d15b045b3a2f51ac2800d848d5883
Zeki Baklan <113357432+zekibaklan@users.noreply.github.com>
2023-09-26 14:00:46 +0300

    Cardview done

---
+++ Honeymoon/Model/HoneymoonModel.swift
@@ -0,0 +1,8 @@
+//
+//  HoneymoonModel.swift
+//  Honeymoon
+//
+//  Created by Zeki Baklan on 26.09.2023.
+//
+
+import Foundation
---
+++ Honeymoon/View/CardView.swift
@@ -0,0 +1,18 @@
+//
+//  CardView.swift
+//  Honeymoon
+//
+//  Created by Zeki Baklan on 26.09.2023.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardView()
+}
