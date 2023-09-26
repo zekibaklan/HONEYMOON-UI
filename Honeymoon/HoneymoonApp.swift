1895cc902f662c56e9ebdbf3d5dc9627e0b52666
Zeki Baklan <113357432+zekibaklan@users.noreply.github.com>
2023-09-26 12:37:55 +0300

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Zeki Baklan on 26.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
