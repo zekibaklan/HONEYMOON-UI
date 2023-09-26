c4add7f62f82ba3ecb91022c760f36bdca0d5342
Zeki Baklan <113357432+zekibaklan@users.noreply.github.com>
2023-09-26 15:56:43 +0300

    GuideComponent is done

---
+++ Honeymoon/View/GuideView.swift
@@ -0,0 +1,18 @@
+//
+//  GuideView.swift
+//  Honeymoon
+//
+//  Created by Zeki Baklan on 26.09.2023.
+//
+
+import SwiftUI
+
+struct GuideView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GuideView()
+}
