f6fce11fd2e35665ee9035b7de93eb8856ac5ee4
Zeki Baklan <113357432+zekibaklan@users.noreply.github.com>
2023-10-01 21:22:57 +0300

    gesture done

---
+++ Honeymoon/Modifier/SymbolModifier.swift
@@ -0,0 +1,18 @@
+//
+//  SymbolModifier.swift
+//  Honeymoon
+//
+//  Created by Zeki Baklan on 1.10.2023.
+//
+
+import SwiftUI
+
+struct SymbolModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SymbolModifier()
+}
